957c7d33958a372c14f024b8d0f12c6679ac3ee5
Marco <mmarino2@gmail.com>
2020-09-08 20:18:56 +0200

    docs agregado

---
+++ docs/precache-manifest.c423f087944bf2800c4e2d5a35495a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4853b827e1eeee5db79cc5bc7b0743",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4812690e4e7fd02ab375",
+    "url": "/static/css/main.89702eb9.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4812690e4e7fd02ab375",
+    "url": "/static/js/main.bc867fde.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
